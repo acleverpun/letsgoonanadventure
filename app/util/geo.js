8b7e3a1b3e1138844164a9f2ec1d628053e668da
Ross Hadden <rosshadden@gmail.com>
2014-12-31 15:46:42 -0500

    Started adding tunnel.

--- app/util/geo.js
+++ app/util/geo.js
@@ -10,8 +10,7 @@ export class Point {
 		if (_.isUndefined(this.mapId)) this.mapId = game.state.getCurrentState().mapId;
 		if (_.isUndefined(this.map)) {
 			let tilemapData = game.cache.getTilemapData(this.mapId);
-			if (_.isNull(tilemapData)) return;
-			this.map = tilemapData.data;
+			if (!_.isNull(tilemapData)) this.map = tilemapData.data;
 		}
 
 		if (_.isString(this.x)) {
@@ -30,10 +29,6 @@ export class Point {
 				this.y = y;
 			}
 		}
-
-		if (!this.tileX && this.x) this.tileX = ~~(this.x / this.map.tilewidth);
-		if (!this.tileY && this.y) this.tileY = ~~(this.y / this.map.tileheight);
-
 		if (_.isString(this.tileX)) {
 			let tileX = +this.tileX;
 			if (_.isNaN(tileX)) {
@@ -51,8 +46,12 @@ export class Point {
 			}
 		}
 
-		if (!this.x && this.tileX) this.x = this.tileX * this.map.tilewidth;
-		if (!this.y && this.tileY) this.x = this.tileY * this.map.tileheight;
+		if (_.isObject(this.map)) {
+			if (!this.tileX && this.x) this.tileX = ~~(this.x / this.map.tilewidth);
+			if (!this.tileY && this.y) this.tileY = ~~(this.y / this.map.tileheight);
+			if (!this.x && this.tileX) this.x = this.tileX * this.map.tilewidth;
+			if (!this.y && this.tileY) this.x = this.tileY * this.map.tileheight;
+		}
 	}
 
 
@@ -66,8 +65,8 @@ export class Point {
 			east: (map) => map.width * tileWidth - tileWidth,
 			west: (map) => 0
 		};
-		log('direction', direction);
 
+		if (!_.isObject(this.map)) throw new Error('Map not yet loaded.');
 		return edgeMap[direction](this.map);
 	}
 
