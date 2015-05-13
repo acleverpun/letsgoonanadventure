14967cb92b9307c909d90d9b2c30c882a374dfb2
Ross Hadden <rosshadden@gmail.com>
2015-05-12 23:25:46 -0400

    Added an interactable nixon.

---
+++ app/nixons/interactable.js
@@ -0,0 +1,27 @@
+import Nixon from './nixon';
+
+
+class Interactable extends Nixon {
+
+	init(...args) {
+		super(...args);
+
+		// default the target entity to the player
+		this.target = this.state.player.sprite;
+	}
+
+
+	update() {
+		let entity = this.entity;
+		let target = this.target;
+
+		// TODO: let nixons apply to specific sprites/groups, instead of just the player
+		this.state.physics.arcade.overlap(target, entity.sprite, function() {
+			entity.emit('enter', target);
+		});
+	}
+
+}
+
+
+export default Interactable;
